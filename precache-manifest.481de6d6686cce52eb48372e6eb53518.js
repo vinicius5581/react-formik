c295dbcbafb6f2e7e10a17b15d8ddcbb58039285
Vinicius Santana <santanaviny@gmail.com>
2018-12-09 13:27:36 -0800

    Updates

---
+++ precache-manifest.481de6d6686cce52eb48372e6eb53518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ea10d5076b83f4fca8",
+    "url": "/react-formik/static/css/main.75a47b0b.chunk.css"
+  },
+  {
+    "revision": "26ea10d5076b83f4fca8",
+    "url": "/react-formik/static/js/main.26ea10d5.chunk.js"
+  },
+  {
+    "revision": "fbce862c350cd72c9f73",
+    "url": "/react-formik/static/js/1.fbce862c.chunk.js"
+  },
+  {
+    "revision": "0e3003a84f673f79ed02",
+    "url": "/react-formik/static/js/runtime~main.0e3003a8.js"
+  },
+  {
+    "revision": "f6ece77ba47434763c2fd5b9ae86d3ee",
+    "url": "/react-formik/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-formik/precache-manifest.481de6d6686cce52eb48372e6eb53518.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-formik/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
